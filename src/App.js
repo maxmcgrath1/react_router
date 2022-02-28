e337f9e77bc55404eaa05800a175b852c19c1bb9
maxmcgrath1 <tuc06096@temple.edu>
2022-02-28 12:13:30 -0500

    Pages are rendering with a navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Currencies from "./pages/currencies";
+import Main from "./pages/main";
+import Price from "./pages/price";
+import Nav from "./components/nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
